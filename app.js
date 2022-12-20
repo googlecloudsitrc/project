f1650a09d8f7e7bbf47ffb2398820a71f7db3626
sanketshivale <shivalesanket21@gmail.com>
2022-12-20 15:33:56 +0530

    Basic Backend Completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const mongoose =require('mongoose')
 
 
@@ -7,6 +8,7 @@ const port = 3000
 const db = "mongodb://localhost:27017/blog"
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
 app.use(express.static('src'))
@@ -36,6 +38,7 @@ app.get("/create", (req, res)=>{
 })
 
 app.post('/create', (req, res)=>{
+    console.log(req.body.postTitle)
     const post =new Post({
         title: req.body.postTitle,
         content: req.body.postBody
